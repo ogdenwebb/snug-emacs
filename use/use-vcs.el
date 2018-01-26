5cc7d1d6c584394a63bc096d1f422d7eaa6bb4c8
ogdenwebb <andrew1byte@gmail.com>
2018-01-27 01:15:22 +0200

    Update use-vcs module.

--- use/use-vcs.el
+++ use/use-vcs.el
@@ -15,6 +15,9 @@
   (setenv "GIT_ASKPASS" "")
   (setenv "SSH_ASKPASS" ""))
 
+(use-package gist
+  :ensure t)
+
 ;; Highlight changes in git
 (use-package git-gutter
   :ensure t
@@ -30,59 +33,11 @@
    '(git-gutter:modified-sign "󠀠")
    '(git-gutter:deleted-sign "󠀠"))
 
-  ;; Color lines
-  ;; (custom-theme-set-faces
-  ;;  'kaolin
-  ;; TODO:
-  ;; (custom-theme-set-faces
-  ;;  'kaolin-eclipse
-  ;;  '(git-gutter:added ((t (:background "#54b685" :foreground "#54b685"))))
-  ;;  ;; '(git-gutter:modified ((t (:background "#54b6b6" :foreground "#54b6b6"))))
-  ;;  '(git-gutter:modified ((t (:background "#ab98b5" :foreground "#ab98b5"))))
-  ;;  '(git-gutter:deleted ((t (:background "#d75f5f" :foreground "#d75f5f")))))
   :config
   (use-package git-gutter-fringe
     :after git-gutter
     :ensure t))
     ;; :config
     ;; (setq git-gutter-fr:side 'right-fringe))
-
-;; (use-package git-gutter
-;;   :commands (global-git-gutter-mode git-gutter-mode)
-;;   :init
-;;   (progn
-;;     (add-hook 'prog-mode-hook 'git-gutter-mode)
-;;     ;; TODO: break down nlinum V
-;;     ;; (git-gutter:linum-setup)
-;;   ;; :config
-;;   ;; TODO: add live reload
-;;   ;; (add-hook 'post-command-hook 'git-gutter:update-all-windows)
-;;   ;; (add-hook 'focus-in-hook 'git-gutter:update-all-windows)
-
-;;     (setq
-;;      git-gutter:handled-backends '(git hg bzr svn)
-;;      git-gutter:update-interval 2
-;;      git-gutter:window-width 1
-;;      git-gutter:diff-option "-w"
-;;      git-gutter:hide-gutter t
-;;      git-gutter:ask-p nil
-;;      git-gutter:verbosity 0)
-
-;;     (custom-set-variables
-;;     ;; WARNING: "" contains tag space character to display line
-;;      '(git-gutter:unchanged-sign "󠀠")
-;;      '(git-gutter:added-sign "󠀠")
-;;      '(git-gutter:modified-sign "󠀠")
-;;      '(git-gutter:deleted-sign "󠀠"))
-
-;;     ;; Color lines
-;;     (custom-set-faces
-;;       `(git-gutter:added ((t (:background "#54b685"))))
-;;       `(git-gutter:modified ((t (:background "#acb370"))))
-;;       `(git-gutter:deleted ((t (:background "#d75f5f"))))))
-;;   :general
-;;   (general-nvmap
-;;    "] h" 'git-gutter:next-hunk
-;;    "[ h" 'git-gutter:previous-hunk))
 
 (provide 'use-vcs)
