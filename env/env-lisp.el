99e3e89393a2af7e8082d80565a72a310a693d03
ogdenwebb <andrew1byte@gmail.com>
2018-02-18 14:24:24 +0200

    Enable eldoc in elisp-mode.

--- env/env-lisp.el
+++ env/env-lisp.el
@@ -1,4 +1,8 @@
 ;; On-the-fly evaluation/substitution of Emacs lisp code
+(use-package elisp-mode
+  :config
+  (add-hook 'emacs-lisp-mode-hook #'eldoc-mode))
+
 (use-package litable
   :mode ("\\.el\\'" . emacs-lisp-mode))
 
