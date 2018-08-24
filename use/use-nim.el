29d8b02dd829b012c3b9a2b8d2efc9d4d7963ac3
ogdenwebb <ogdenwebb@gmail.com>
2018-08-24 16:15:09 +0300

    Add nimsuggest-delete-home-logfile.

--- use/use-nim.el
+++ use/use-nim.el
@@ -5,4 +5,14 @@
   (add-hook 'nim-mode-hook 'flycheck-mode)
   (add-hook 'nim-mode-hook 'nimsuggest-mode))
 
+(defun nimsuggest-delete-home-logfile ()
+  "Delete nimsuggest log file in $HOME directory."
+  (let ((nimsuggest-file (concat (getenv "HOME") "/nimsuggest.log")))
+    (when (file-exists-p nimsuggest-file)
+      (delete-file nimsuggest-file))))
+
+(add-hook 'nim-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook 'nimsuggest-delete-home-logfile nil 'make-it-local)))
+
 (provide 'use-nim)
