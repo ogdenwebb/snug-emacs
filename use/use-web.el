e4176b35c2fdb4552d4f2e0315b7681c2b6f4cb6
ogdenwebb <ogdenwebb@gmail.com>
2019-01-14 18:56:34 +0200

    Update indent settings.

--- use/use-web.el
+++ use/use-web.el
@@ -4,6 +4,14 @@
   :mode (("\\.html\\'" . web-mode))
   :config
   (add-hook 'web-mode-hook 'turn-off-smartparens-mode)
+  ;; For emmet-mode to switch between HTML and CSS
+  (add-hook 'web-mode-before-auto-complete-hooks
+            '(lambda ()
+               (let ((web-mode-cur-lang
+                      (web-mode-language-at-pos)))
+                 (if (string= web-mode-cur-lang "css")
+                     (setq emmet-use-css-transform t)
+                   (setq emmet-use-css-transform nil)))))
 
   ;; (add-hook 'web-mode-hook #'(lambda () (highlight-indent-guides-mode -1)))
   ;; (add-hook 'web-mode-hook #'(lambda () (yas-activate-extra-mode 'web-mode)))
@@ -11,15 +19,20 @@
   ;; Install tidy html5
   ;; (flycheck-add-mode 'html-tidy 'web-mode)
 
-  (setq web-mode-markup-indent-offset 2
-        web-mode-css-indent-offset 2
+  (setq web-mode-attr-indent-offset 2
         web-mode-code-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-indent-style 2
+        web-mode-markup-indent-offset 2
+        web-mode-sql-indent-offset 2
+
         ;; web-mode-enable-auto-closing t
         ;; web-mode-enable-auto-pairing t
         web-mode-enable-auto-opening t
         web-mode-enable-comment-interpolation t
         web-mode-enable-css-colorization t
         web-mode-enable-auto-indentation t
+
         web-mode-imenu-regexp-list
         '(("<\\(h[1-9]\\)\\([^>]*\\)>\\([^<]*\\)" 1 3 ">" nil)
           ("^[ \t]*<\\([@a-z]+\\)[^>]*>? *$" 1 " id=\"\\([a-zA-Z0-9_]+\\)\"" "#" ">")
@@ -42,7 +55,8 @@
 (use-package company-web-html
   :after (company web-mode)
   :config
-  (add-to-list 'company-backends '(company-web-html :with company-etags)))
+  ;; (add-to-list 'company-backends '(company-web-html :with company-etags)))
+  (add-to-list 'company-backends '(company-css company-web-html company-files)))
 
 
 (provide 'use-web)
