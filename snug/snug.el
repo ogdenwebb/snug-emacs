b4bd0f5cae2b997b8b730f8a01f71af982da5ce7
ogdenwebb <ogdenwebb@gmail.com>
2019-03-15 22:09:36 +0200

    Move straight org-mode block to snug.el file.

--- snug/snug.el
+++ snug/snug.el
@@ -120,6 +120,38 @@ i.e. :keyword to \"keyword\"."
 ;;                               (time-subtract after-init-time before-init-time)))
 ;;                      gcs-done)))
 
+
+;; Use git version of Org-mode
+;; https://github.com/raxod502/straight.el#installing-org-with-straightel
+(defun org-git-version ()
+  "The Git version of org-mode.
+Inserted by installing org-mode or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (git-run "describe"
+              "--match=release\*"
+              "--abbrev=6"
+              "HEAD"))))
+
+(defun org-release ()
+  "The release version of org-mode.
+Inserted by installing org-mode or when a release is made."
+  (require 'git)
+  (let ((git-repo (expand-file-name
+                   "straight/repos/org/" user-emacs-directory)))
+    (string-trim
+     (string-remove-prefix
+      "release_"
+      (git-run "describe"
+               "--match=release\*"
+               "--abbrev=0"
+               "HEAD")))))
+
+(provide 'org-version)
+(straight-use-package 'org)
+
 (require 'snug-core)
 (require 'snug-settings)
 
