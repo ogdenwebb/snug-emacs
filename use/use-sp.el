8dae373d8290926e7247999c0b69812a4edc4385
ogdenwebb <ogdenwebb@gmail.com>
2019-07-01 03:23:43 +0300

    Hook for smartparens.

--- use/use-sp.el
+++ use/use-sp.el
@@ -1,5 +1,6 @@
 ;; -*- lexical-binding: t -*-
 (use-package smartparens
+  :hook (after-init . smartparens-global-mode)
   :config
   (require 'smartparens-config)
   ;; (setq sp-autowrap-region nil ; let evil-surround handle this
