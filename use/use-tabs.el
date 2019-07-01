b8740e1b8bd07726e03888ec49753195b4370956
ogdenwebb <ogdenwebb@gmail.com>
2019-07-01 03:21:46 +0300

    Update use-tabs.

--- use/use-tabs.el
+++ use/use-tabs.el
@@ -5,4 +5,16 @@
   (awesome-tab-mode t)
 )
 
+(use-package centaur-tabs
+  :disabled t
+  :straight (:host github :repo "ema2159/centaur-tabs")
+   :config
+   (setq centaur-tabs-background-color (face-background 'centaur-tabs-default))
+   (setq centaur-tabs-set-close-button nil)
+   (setq centaur-tabs-style "bar"
+         centaur-tabs-height 32
+         centaur-tabs-set-icons t)
+   (centaur-tabs-mode t)
+   )
+
 (provide 'use-tabs)
