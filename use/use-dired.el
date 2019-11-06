7f799e02b343266848f075903acb116e6588de95
ogdenwebb <ogdenwebb@gmail.com>
2019-11-06 13:52:28 +0200

    Enable diredfl.

--- use/use-dired.el
+++ use/use-dired.el
@@ -1,5 +1,7 @@
 ;;; Dired file browser -*- lexical-binding: t -*-
 
-(use-package dired+)
+;; (use-package dired+)
+(use-package diredfl
+  :hook (dired-mode . diredfl-mode))
 
 (provide 'use-dired)
