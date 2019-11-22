e0c2daf51665f8ef17155616a300d669f2ba9ec1
ogdenwebb <ogdenwebb@gmail.com>
2019-11-22 23:59:19 +0200

    Update dired config.

--- use/use-dired.el
+++ use/use-dired.el
@@ -1,5 +1,20 @@
 ;;; Dired file browser -*- lexical-binding: t -*-
 
+;; Open file in dired with xdg-open
+(use-package dired
+  :straight nil
+  :config
+  (defun dired-open-file ()
+    "In dired, open the file named on this line."
+    (interactive)
+    (let* ((file (dired-get-filename nil t)))
+      (message "Opening %s..." file)
+      (call-process "xdg-open" nil 0 nil file)
+      (message "Opening %s done" file))
+
+    ;; select file or directory.
+    (define-key dired-mode-map (kbd "RET") 'dired-open-file)))
+
 ;; (use-package dired+)
 (use-package diredfl
   :defer t
