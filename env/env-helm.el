cdd37b77d51696340f36a9d379881846da3b1505
ogdenwebb <ogdenwebb@gmail.com>
2019-11-30 20:32:07 +0200

    Enable helm fuzzy stuff. Settings for helm.

--- env/env-helm.el
+++ env/env-helm.el
@@ -2,20 +2,35 @@
 
 (use-package helm
   :defer t
-  :hook (after-init . helm-mode)
+  :hook ((after-init . helm-mode)
+         (helm-mode . helm-autoresize-mode))
   :config
   ;; No idea why here find-file is set to nil (so it uses the native find-file
   ;; for Emacs. This makes stuff like (find-file (read-file-name ...)) work with
   ;; Helm again.
   ;; (add-to-list 'helm-completing-read-handlers-alist '(find-file . helm-completing-read-symbols))
-  ;; Configure helm
-  ;; (setq helm-display-buffer-height 12)
+
+  (use-package helm-config
+               :straight nil)
 
   ;; Fuzzy stuff
-  ;; (setq helm-M-x-fuzzy-match t
-  ;;       helm-buffers-fuzzy-matching t
-  ;;       helm-recentf-fuzzy-match t
-  ;;       helm-lisp-fuzzy-completion t)
+  (setq helm-M-x-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match t
+        helm-lisp-fuzzy-completion t)
+
+  ;; Customize Helm
+  (setq helm-split-window-inside-p t
+        helm-ff-search-library-in-sexp t        ; search for library in `require' and `declare-function' sexp.
+        helm-ff-file-name-history-use-recentf t ; Use `recentf-list' instead of `file-name-history'.
+        helm-display-header-line nil
+        helm-display-buffer-height 30
+        helm-autoresize-max-height 30
+        helm-autoresize-min-height 30)
+
+  ;; Disable helm mode-line
+  ;; (setq-default helm-mode-line-string nil)
+  ;; (setq mode-line-format (default-value 'mode-line-format))
 
   ;; Set helm as default completion system
   (with-eval-after-load 'projectile
@@ -35,11 +50,6 @@
   :after helm
   :hook (helm-mode . helm-descbinds-mode))
 
-;; Rifle through your Org-mode buffers and acquire your target
-(use-package helm-org-rifle
-  :defer t
-  :after helm)
-
 (use-package helm-swoop
   :defer t
   :after helm)
@@ -49,6 +59,20 @@
   :defer t
   :after helm)
 
-(use-package helm-ls-git)
+(use-package helm-ls-git
+  :defer t
+  :after helm)
+
+(use-package swiper-helm
+  :after helm)
+
+(use-package helm-org
+  :after (helm org))
+
+;; Rifle through your Org-mode buffers and acquire your target
+(use-package helm-org-rifle
+  :defer t
+  :after helm)
+
 
 (provide 'env-helm)
