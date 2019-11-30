0fa0476c88cbd001926c2ef9f3a622716f90f8a1
ogdenwebb <ogdenwebb@gmail.com>
2019-11-30 20:29:45 +0200

    Setup eshell.

--- use/use-eshell.el
+++ use/use-eshell.el
@@ -5,6 +5,25 @@
         (lambda nil
           (concat
            (eshell/pwd)
-           " ~ ❯ "))))
+           " ~ ❯ "))
+        eshell-bad-command-tolerance 1
+        eshell-cmpl-autolist t
+        eshell-cmpl-cycle-completions nil
+        eshell-cmpl-cycle-cutoff-length 2
+        eshell-cmpl-ignore-case t
+        eshell-cp-overwrite-files nil
+        eshell-default-target-is-dot t
+
+        eshell-destroy-buffer-when-process-dies t
+        eshell-hist-ignoredups t
+        eshell-list-files-after-cd t
+        eshell-review-quick-commands t
+        eshell-save-history-on-exit t
+        eshell-scroll-show-maximum-output nil
+        ;; eshell-stringify nil
+        ;; TODO: explore
+        eshell-visual-options nil
+
+        ))
 
 (provide 'use-eshell)
