aded99646c867b301d3351f14219918be99dbef2
ogdenwebb <ogdenwebb@gmail.com>
2020-02-28 14:02:08 +0200

    Use org-plus-contrib.

--- use/use-org.el
+++ use/use-org.el
@@ -1,90 +1,25 @@
 ;;; Org-mode settings  -*- lexical-binding: t -*-
 
+
+;; ;; Split current tree into new buffer
+;; (defun split-and-indirect-orgtree ()
+;;   "Splits window to the right and opens an org tree section in it"
+;;   (interactive)
+;;   (split-window-right)
+;;   (org-tree-to-indirect-buffer)
+;;   (windmove-right))
+
+;; (evil-define-key 'insert org-mode-map
+;;   (kbd "M-j") 'org-shiftleft
+;;   (kbd "M-k") 'org-shiftright
+;;   (kbd "M-H") 'org-metaleft
+;;   (kbd "M-J") 'org-metadown
+;;   (kbd "M-K") 'org-metaup
+;;   (kbd "M-L") 'org-metaright)
+
 ;; TODO: use org-plus-contrib
 ;; prolly need hook instead of :config
-(use-package org
- ;; use-package org-plus-contrib
-  :config
-
-  ;; (setq org-directory "~/Drive/org")
-  ;; Fit image into the screen
-  ;; (setq org-image-actual-width '(600))
-  (setq org-image-actual-width nil)
-  ;; (setq org-image-actual-width 200)
-  (setq org-startup-with-inline-images t)
-  ;; (setq org-image-actual-width (/ (display-pixel-width) 3))
-  (setq org-startup-indented t
-        org-startup-folded t
-        org-imenu-depth 4
-        org-tags-column 0)
-
-  ;; todo:
-  ;; (setq org-startup-truncated nil)
-  ;; Agenda
-  (setq org-log-done t)
-  (setq org-agenda-files '("~/agenda.org"))
-
-  ;; Fontify
-  (setq org-fontify-done-headline t
-        org-fontify-quote-and-verse-blocks t
-        org-src-fontify-natively t
-        org-src-tab-acts-natively t
-        ;; org-pretty-entities nil
-        org-hide-emphasis-markers t
-        org-hide-leading-stars t
-        org-hide-leading-stars-before-indent-mode t
-        org-ellipsis " ~ ")
-
-  (setq org-adapt-indentation nil)
-  ;; Fontify the whole line for headings (with a background color).
-  (setq org-fontify-whole-heading-line t)
-
-  (defun snug/org-init-hook ()
-    (interactive)
-    ;; Set cursor-type for org-mode
-    ;; (setq cursor-type 'bar)
-
-    (when (bound-and-true-p nlinum-mode)
-      (nlinum-mode -1))
-    (when (bound-and-true-p display-line-numbers-mode)
-      (display-line-numbers-mode -1))
-    ;; TODO: disable git-gutter
-    ;; Top padding
-    ;; (setq header-line-format " ")
-    ;; Enable line wrapping
-    (visual-line-mode t)
-    (turn-off-smartparens-mode)
-    (show-paren-mode -1))
-
-  (add-hook 'org-mode-hook 'snug/org-init-hook)
-
-  (setq org-highest-priority ?A
-        org-lowest-priority ?D
-        org-default-priority ?B)
-
-  (custom-theme-set-faces
-   ;; snug-custom-theme
-   'user
-   '(org-done ((t (:foreground "dimgray" :bold t :strike-through t))))
-   '(org-headline-done ((t (:foreground "dimgray" :bold nil :strike-through t)))))
-
-  ;; Fontify done checkbox items in org-mode
-  (font-lock-add-keywords
-   'org-mode
-   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)" 1 'org-headline-done prepend))
-   'append)
-
-  ;; Open links
-  (setq org-link-frame-setup '((vm . vm-visit-folder)
-                               (gnus . org-gnus-no-new-news)
-                               (file . find-file)))
-
-  (setq org-src-window-setup 'current-window)
-
-  ;; Customize org todo keywrods
-  (setq org-todo-keywords '((sequence "TODO(t)" "EXPLORE(e)" "ACTIVE(a)" "|" "DONE(d)" "CANCELED(c)")))
-  ;; (setq org-fast-tag-selection-include-todo t)
-
+(use-package org-plus-contrib
   :general
   (general-define-key :keymaps 'org-mode-map
                       :states '(normal)
@@ -120,22 +55,86 @@
                       "RET" 'org-return))
                       ;; "RET" 'org-return-indent))
 
+(defun snug/org-init-hook ()
+  ;; (setq org-directory "~/Drive/org")
+  ;; Fit image into the screen
+  ;; (setq org-image-actual-width '(600))
+  (setq org-image-actual-width nil)
+  ;; (setq org-image-actual-width 200)
+  (setq org-startup-with-inline-images t)
+  ;; (setq org-image-actual-width (/ (display-pixel-width) 3))
+  (setq org-startup-indented t
+        org-startup-folded t
+        org-imenu-depth 4
+        org-tags-column 0)
 
-;; ;; Split current tree into new buffer
-;; (defun split-and-indirect-orgtree ()
-;;   "Splits window to the right and opens an org tree section in it"
-;;   (interactive)
-;;   (split-window-right)
-;;   (org-tree-to-indirect-buffer)
-;;   (windmove-right))
+  ;; todo:
+  ;; (setq org-startup-truncated nil)
+  ;; Agenda
+  (setq org-log-done t
+        org-agenda-files '("~/agenda.org"))
 
-;; (evil-define-key 'insert org-mode-map
-;;   (kbd "M-j") 'org-shiftleft
-;;   (kbd "M-k") 'org-shiftright
-;;   (kbd "M-H") 'org-metaleft
-;;   (kbd "M-J") 'org-metadown
-;;   (kbd "M-K") 'org-metaup
-;;   (kbd "M-L") 'org-metaright)
+  ;; Fontify
+  (setq org-fontify-done-headline t
+        org-fontify-quote-and-verse-blocks t
+        org-src-fontify-natively t
+        org-src-tab-acts-natively t
+        ;; org-pretty-entities nil
+        org-hide-emphasis-markers t
+        org-hide-leading-stars t
+        org-hide-leading-stars-before-indent-mode t
+        org-ellipsis " ~ ")
+
+  (setq org-adapt-indentation nil)
+  ;; Fontify the whole line for headings (with a background color).
+  (setq org-fontify-whole-heading-line t)
+
+  (setq org-highest-priority ?A
+        org-lowest-priority ?D
+        org-default-priority ?B)
+
+  (custom-theme-set-faces
+   ;; snug-custom-theme
+   'user
+   '(org-done ((t (:foreground "dimgray" :bold t :strike-through t))))
+   '(org-headline-done ((t (:foreground "dimgray" :bold nil :strike-through t)))))
+
+  ;; Fontify done checkbox items in org-mode
+  (font-lock-add-keywords
+   'org-mode
+   `(("^[ \t]*\\(?:[-+*]\\|[0-9]+[).]\\)[ \t]+\\(\\(?:\\[@\\(?:start:\\)?[0-9]+\\][ \t]*\\)?\\[\\(?:X\\|\\([0-9]+\\)/\\2\\)\\][^\n]*\n\\)" 1 'org-headline-done prepend))
+   'append)
+
+  ;; Open links
+  (setq org-link-frame-setup '((vm . vm-visit-folder)
+                               (gnus . org-gnus-no-new-news)
+                               (file . find-file)))
+
+  (setq org-src-window-setup 'current-window)
+
+  ;; Customize org todo keywrods
+  (setq org-todo-keywords '((sequence "TODO(t)" "EXPLORE(e)" "ACTIVE(a)" "|" "DONE(d)" "CANCELED(c)"))))
+  ;; (setq org-fast-tag-selection-include-todo t))
+
+
+(defun snug/org-visual-hook ()
+  ;; Set cursor-type for org-mode
+  ;; (setq cursor-type 'bar)
+
+  (when (bound-and-true-p nlinum-mode)
+    (nlinum-mode -1))
+  (when (bound-and-true-p display-line-numbers-mode)
+    (display-line-numbers-mode -1))
+  ;; TODO: disable git-gutter
+  ;; Top padding
+  ;; (setq header-line-format " ")
+  ;; Enable line wrapping
+  (visual-line-mode t)
+  (turn-off-smartparens-mode)
+  (show-paren-mode -1))
+
+(add-hook 'org-mode-hook 'snug/org-init-hook)
+(add-hook 'org-mode-hook 'snug/org-visual-hook)
 
 (use-package org-bullets
   ;; :requires org
