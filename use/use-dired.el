c81fcf1c2812eb31565f1c50c1c47ccbcd1517cd
ogdenwebb <ogdenwebb@gmail.com>
2020-03-27 19:27:25 +0200

    More dired related stuff.

--- use/use-dired.el
+++ use/use-dired.el
@@ -1,12 +1,18 @@
 ;;; Dired file browser -*- lexical-binding: t -*-
 
-;; Open file in dired with xdg-open
+;; Dired settings
 (use-package dired
   :straight nil
+  :hook (dired-mode . hl-line-mode)
   :config
   ;; Dired settings
   (setq dired-hide-details-hide-symlink-targets nil
-        dired-omit-verbose nil)
+        dired-omit-verbose nil
+        dired-listing-switches "-AFhlv --group-directories-first"
+        dired-dwim-target t
+        dired-recursive-copies 'always
+        ;; dired-recursive-deletes 'always
+        )
 
   (defun dired-open-file ()
     "In dired, open the file named on this line."
@@ -19,9 +25,20 @@
     ;; select file or directory.
     (define-key dired-mode-map (kbd "RET") 'dired-open-file)))
 
-;; (use-package dired+)
+(use-package find-dired
+  :after dired
+  :config
+  (setq find-ls-option
+        '("-ls" . "-AFhlv --group-directories-first")
+        find-name-arg "-iname"))
+
+;; Extra font lock rules for a more colourful dired
 (use-package diredfl
   :defer t
   :hook (dired-mode . diredfl-mode))
+
+(use-package dired-async
+  :after (dired async)
+  :hook (dired-mode . dired-async-mode))
 
 (provide 'use-dired)
