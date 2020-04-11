7ed26c79b4ef5e1640ac64861cffd96bd6754263
ogdenwebb <ogdenwebb@gmail.com>
2020-04-11 03:24:58 +0300

    Flycheck-haskell.

--- use/use-haskell.el
+++ use/use-haskell.el
@@ -4,6 +4,10 @@
   :config
   (add-hook 'haskell-mode-hook (lambda () (setq-local electric-indent-inhibit t))))
 
+(use-package flycheck-haskell
+  :defer t
+  :hook (haskell-mode . flycheck-haskell-setup))
+
 (use-package dante
   :after haskell-mode
   :disabled t
