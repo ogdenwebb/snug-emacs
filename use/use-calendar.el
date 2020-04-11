95349c5a52b86f0f451d6433c7ea5605b75fbc33
ogdenwebb <ogdenwebb@gmail.com>
2020-04-11 03:22:35 +0300

    Setup default calendar.

--- use/use-calendar.el
+++ use/use-calendar.el
@@ -1,7 +1,30 @@
+;; Calendar module -*- lexical-binding: t; -*-
+
+(use-package calendar
+  :straight nil
+  :hook (calendar-today-visible . calendar-mark-today)
+  :config
+  (setq calendar-time-display-form
+        '(24-hours ":" minutes
+                   (when time-zone
+                     (concat " (" time-zone ")")))
+        calendar-week-start-day 1 ; Monday
+        calendar-date-style 'iso
+        ;; calendar-mark-diary-entries-flag t
+        calendar-holidays
+        (append holiday-general-holidays holiday-local-holidays
+                holiday-other-holidays holiday-christian-holidays
+                holiday-islamic-holidays holiday-oriental-holidays
+                holiday-solar-holidays))
+  )
+
+
+;; Calfw - A calendar framework for Emacs
 (use-package calfw
   :defer t
+  :preface
+  (defalias 'cal 'cfw:open-calendar-buffer)
   :commands (cfw:open-calendar-buffer))
 
-(defalias 'cal 'cfw:open-calendar-buffer)
 
 (provide 'use-calendar)
