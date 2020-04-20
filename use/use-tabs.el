70e80d16376ee40be9432ca7decf878e3a095136
ogdenwebb <ogdenwebb@gmail.com>
2020-04-20 20:03:09 +0300

    Update centaur-tabs settings.

--- use/use-tabs.el
+++ use/use-tabs.el
@@ -14,13 +14,17 @@
   :straight (:host github :repo "ema2159/centaur-tabs")
   :hook (after-init . centaur-tabs-mode)
   :config
-  (setq centaur-tabs-background-color (face-background 'centaur-tabs-default))
-  (setq centaur-tabs-set-close-button nil)
-  (setq centaur-tabs-style "bar"
+  (setq centaur-tabs-background-color (face-background 'centaur-tabs-default)
+        centaur-tabs-set-close-button nil
+        centaur-tabs-style "wave"
+
         ;; centaur-tabs-set-bar 'left
         centaur-tabs-height 32
-        centaur-tabs-set-icons t)
+        centaur-tabs-set-icons t
+        ;; centaur-tabs-gray-out-icons 'buffer
+        ;; centaur-tabs-plain-icons t)
         ;; centaur-tabs-set-modified-marker t)
+        )
   )
 
 (provide 'use-tabs)
