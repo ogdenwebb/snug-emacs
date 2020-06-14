985083e1561f0264a66fcacd858f2c4a07d1afc7
ogdenwebb <ogdenwebb@gmail.com>
2020-06-15 01:05:49 +0300

    Rename perl6 module to raku.

--- use/use-perl6.el
+++
@@ -1,8 +0,0 @@
-(use-package perl6-detect
-  :straight (:host github :repo "perl6/perl6-mode"))
-
-(use-package flycheck-perl6
-  :after perl6-mode
-  :hook (perl6-mode . flycheck-mode))
-
-(provide 'use-perl6)
---
+++ use/use-raku.el
@@ -0,0 +1,8 @@
+(use-package raku-mode
+  :defer t)
+
+;; (use-package flycheck-perl6
+;;   :after perl6-mode
+;;   :hook (perl6-mode . flycheck-mode))
+
+(provide 'use-raku)
