8fbe0bce0aede1af9426db839e8c7dd5e0c26272
ogdenwebb <ogdenwebb@gmail.com>
2020-08-28 11:22:22 +0300

    Selectrum imenu support.

--- env/env-selectrum.el
+++ env/env-selectrum.el
@@ -297,4 +297,38 @@ Depends on `projectile'."
              (user-error "Not in project")))))
   )
 
+(defvar selectrum-imenu+ nil)
+
+(defun sel/imenu ()
+  "Choose from `imenu' just like `counsel-imenu'."
+  (interactive)
+  (require 'imenu)
+  (let* ((selectrum-should-sort-p nil)
+         (candidates (let* ((imenu-auto-rescan t)
+                            (items (imenu--make-index-alist t)))
+                       ;; remove *Rescan*
+                       (setq items (delete (assoc "*Rescan*" items) items))
+                       ;; special mode
+                       (when (eq major-mode 'emacs-lisp-mode)
+                         (let ((fns (cl-remove-if #'listp items :key #'cdr)))
+                           (if fns (setq items (nconc (cl-remove-if #'nlistp items :key #'cdr) `(("Functions" ,@fns)))))))
+                       ;; refine
+                       (cl-labels ((get-candidates (alist &optional prefix)
+                                     (cl-mapcan
+                                      (lambda (elm)
+                                        (if (imenu--subalist-p elm)
+                                            (get-candidates
+                                             (cl-loop for (e . v) in (cdr elm)
+                                                   collect (cons e (if (integerp v) (copy-marker v) v)))
+                                             (concat prefix (if prefix ".") (car elm)))
+                                          (let ((key (concat (if prefix (concat (propertize prefix 'face 'font-lock-keyword-face) ": "))
+                                                             (car elm))))
+                                            (list (cons key (cons key (if (overlayp (cdr elm)) (overlay-start (cdr elm)) (cdr elm))))))))
+                                      alist)))
+                         (setq items (get-candidates items)))
+                       ;; sort
+                       (cl-sort items #'string< :key #'car)))
+         (cand (completing-read "Imenu: " (mapcar #'car candidates) nil t nil selectrum-imenu+)))
+    (imenu (cdr (cl-find cand candidates :test #'string= :key #'car)))))
+
 (provide 'env-selectrum)
