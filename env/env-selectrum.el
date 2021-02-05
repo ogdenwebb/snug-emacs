9d9c6a5787110449e3df48c9899701d099542c0b
ogdenwebb <ogdenwebb@gmail.com>
2021-02-05 22:25:39 +0200

    Add settings for embark-consult.

--- env/env-selectrum.el
+++ env/env-selectrum.el
@@ -19,9 +19,7 @@
 
 (use-package consult-selectrum
   :straight nil
-  :after consult
-
-  )
+  :after consult)
 
 (use-package marginalia
   :after selectrum
@@ -32,9 +30,17 @@
 
   (marginalia-mode))
 
-;; TODO
+;; TODO provides ivy-occur alternative
 ;; (use-package embark)
-;; (use-package embark-consult)
+
+;; Consult users will also want the embark-consult package.
+;; (use-package embark-consult
+;;   :after (embark consult)
+;;   :demand t ; only necessary if you have the hook below
+;;   ;; if you want to have consult previews as you move around an
+;;   ;; auto-updating embark collect buffer
+;;   :hook
+;;   (embark-collect-mode . embark-consult-preview-minor-mode))
 
 (use-package ctrlf
   :defer .5
