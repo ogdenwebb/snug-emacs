a4535c78de183e70f8cd0aabca17cab9941f9c1e
ogdenwebb <ogdenwebb@gmail.com>
2021-03-12 08:53:03 +0200

    Update TLS settings.

--- snug/snug.el
+++ snug/snug.el
@@ -64,12 +64,30 @@
       straight-recipes-emacsmirror-use-mirror t)
 
 ;; Security settings
-(setq gnutls-verify-error (not (getenv "INSECURE")) ; you shouldn't use this
+;; Emacs is essentially one huge security vulnerability, what with all the
+;; dependencies it pulls in from all corners of the globe. Let's try to be at
+;; least a little more discerning.
+(setq gnutls-verify-error (not (getenv-internal "INSECURE"))
+      gnutls-algorithm-priority
+      (when (boundp 'libgnutls-version)
+        (concat "SECURE128:+SECURE192:-VERS-ALL"
+                (if (and (not (version< emacs-version "26.3"))
+                         (>= libgnutls-version 30605))
+                    ":+VERS-TLS1.3")
+                ":+VERS-TLS1.2"))
+      ;; `gnutls-min-prime-bits' is set based on recommendations from
+      ;; https://www.keylength.com/en/4/
+      gnutls-min-prime-bits 3072
       tls-checktrust gnutls-verify-error
-      tls-program (list "gnutls-cli --x509cafile %t -p %p %h"
-                        ;; compatibility fallbacks
-                        "gnutls-cli -p %p %h"
-                        "openssl s_client -connect %h:%p -no_ssl2 -no_ssl3 -ign_eof"))
+  ;; Emacs is built with `gnutls' by default, so `tls-program' would not be
+  ;; used in that case. Otherwise, people have reasons to not go with
+  ;; `gnutls', we use `openssl' instead. For more details, see
+  ;; https://redd.it/8sykl1
+      tls-program '("openssl s_client -connect %h:%p -CAfile %t -nbio -no_ssl3 -no_tls1 -no_tls1_1 -ign_eof"
+                    "gnutls-cli -p %p --dh-bits=3072 --ocsp --x509cafile=%t \
+--strict-tofu --priority='SECURE192:+SECURE128:-VERS-ALL:+VERS-TLS1.2:+VERS-TLS1.3' %h"
+                    ;; compatibility fallbacks
+                    "gnutls-cli -p %p %h"))
 
 (straight-use-package 'use-package)
 (setq straight-use-package-by-default t
