5a83c2bc219cc450811bc798780ea6796734fed3
ogdenwebb <ogdenwebb@gmail.com>
2021-05-04 19:58:55 +0300

    Update centaur-tabs settings

--- use/use-tabs.el
+++ use/use-tabs.el
@@ -1,21 +1,17 @@
-(use-package awesome-tab
-  :disabled t
-  :load-path "dev/awesome-tab"
-  :config
-  (awesome-tab-mode t))
+;;; Tabs -*- lexical-binding: t -*-
 
-(use-package powerline
-  :defer t)
+;; (use-package powerline
+;;   :defer t)
 
 (use-package centaur-tabs
-  :straight (:host github :repo "ema2159/centaur-tabs")
-  :hook ((after-init . centaur-tabs-mode)
-         (imenu-list-major-mode . centaur-tabs-local-mode))
-  :init
-  (setq-default centaur-tabs-set-bar 'under)
+  ;; :hook ((imenu-list-major-mode . centaur-tabs-local-mode))
   :config
+  (centaur-tabs-mode t)
+  ;; (centaur-tabs-headline-match)
+
+
   (setq centaur-tabs-set-close-button nil
-        centaur-tabs-background-color (face-background 'centaur-tabs-default)
+        ;; centaur-tabs-background-color (face-background 'centaur-tabs-default)
         ;; centaur-tabs-style "wave"
 
         ;; centaur-tabs-style "bar"
@@ -27,5 +23,22 @@
         centaur-tabs-modified-marker "*"
         centaur-tabs-plain-icons t
         centaur-tabs-set-modified-marker t))
+
+;; Create `after-load-theme-hook'.
+;; TODO submit this upstream?
+;; (defvar after-load-theme-hook nil
+;;   "Hook run after a color theme is loaded using `load-theme'.")
+
+;; (defadvice load-theme (after run-after-load-theme-hook activate)
+;;   "Run `after-load-theme-hook'."
+;;   (run-hooks 'after-load-theme-hook))
+
+;; (defun reset-tabs-background ()
+;;   "test"
+;;   (interactive)
+;;   (setq centaur-tabs-background-color (face-background 'default)))
+
+;; (add-hook 'after-load-theme-hook #'reset-tabs-background)
+;; (add-hook 'after-load-theme-hook #'centaur-tabs-display-update)
 
 (provide 'use-tabs)
