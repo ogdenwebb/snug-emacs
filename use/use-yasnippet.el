ae97e483e4c210680b10b8b4019cb9688ee71200
ogdenwebb <ogdenwebb@gmail.com>
2021-05-04 19:59:24 +0300

    Note extra yasnippets settings

--- use/use-yasnippet.el
+++ use/use-yasnippet.el
@@ -3,6 +3,9 @@
   :diminish yas-minor-mode
   :hook (after-init . yas-global-mode)
   :config
+  ;; (setq yas-verbosity nil
+  ;;       yas-also-auto-indent-first-line t
+  ;;       yas-wrap-around-region t)
   (use-package yasnippet-snippets))
 
 (provide 'use-yasnippet)
