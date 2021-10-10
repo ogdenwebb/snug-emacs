3a947cdc57c0ff1b1f61d71534e262149c358edb
ogdenwebb <ogdenwebb@gmail.com>
2021-10-10 21:11:01 +0300

    Use single setq
    May or may not make minor improvement in startup time

--- early-init.el
+++ early-init.el
@@ -5,29 +5,41 @@
 
 ;; Defer garbage collection further back in the startup process
 (setq gc-cons-threshold most-positive-fixnum
-      gc-cons-percentage 0.6)
+      gc-cons-percentage 0.6
 
-;; In noninteractive sessions, prioritize non-byte-compiled source files to
-;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
-;; to skip the mtime checks on every *.elc file.
-;; (setq load-prefer-newer 'noninteractive)
+      ;; In noninteractive sessions, prioritize non-byte-compiled source files to
+      ;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+      ;; to skip the mtime checks on every *.elc file.
+      ;; load-prefer-newer 'noninteractive
 
-;; In Emacs 27+, package initialization occurs before `user-init-file' is
-;; loaded, but after `early-init-file'.
-(setq package-enable-at-startup nil)
+      ;; In Emacs 27+, package initialization occurs before `user-init-file' is
+      ;; loaded, but after `early-init-file'.
+      package-enable-at-startup nil
 
-(advice-add #'package--ensure-init-file :override #'ignore) ; DEPRECATED Removed in 28
+      ;; Prevent unwanted runtime compilation for gccemacs (native-comp) users;
+      ;; packages are compiled ahead-of-time when they are installed and site files
+      ;; are compiled when gccemacs is installed.
+      ;; REVIEW Remove after a month
+      comp-deferred-compilation nil
+      native-comp-deferred-compilation nil
+      package-native-compile t
 
-;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-(setq default-frame-alist
+      ;; Disable built-in mode-line because we have own config
+      mode-line-format nil
+
+      ;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+      default-frame-alist
       '((vertical-scroll-bars . nil)
         (menu-bar-lines . 0)
-        (tool-bar-lines . 0)))
+        (tool-bar-lines . 0))
 
-;; Resizing the Emacs frame can be a terribly expensive part of changing the
-;; font. By inhibiting this, we easily halve startup times with fonts that are
-;; larger than the system default.
-(setq frame-inhibit-implied-resize t)
+      ;; Resizing the Emacs frame can be a terribly expensive part of changing the
+      ;; font. By inhibiting this, we easily halve startup times with fonts that are
+      ;; larger than the system default.
+      frame-inhibit-implied-resize t)
+
+
+(advice-add #'package--ensure-init-file :override #'ignore) ; DEPRECATED Removed in 28
 
 ;; Ignore X resources; its settings would be redundant with the other settings
 ;; in this file and can conflict with later config (particularly where the
