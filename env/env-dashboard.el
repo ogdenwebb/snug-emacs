e9193c02066d2afbfce362395738b72dfaf3a1a4
ogdenwebb <ogdenwebb@gmail.com>
2023-09-28 03:46:09 +0300

    Update dashboard module

--- env/env-dashboard.el
+++ env/env-dashboard.el
@@ -1,43 +1,48 @@
 ;; Dashboard
-;; TODO: add evil binding
 (use-package dashboard
-  :disabled t
   :config
-  ;; (use-package dashboard-project-status
-  ;;  :config
-  ;;   (add-to-list 'dashboard-item-generators
-  ;;                `(project-status . ,(dashboard-project-status "~/.snug")))
-  ;;   (add-to-list 'dashboard-items '(project-status) t)
-  ;;   (setq dashboard-items '((project-status . 10)
-  ;;                           (recents        . 10))))
-  ;;                           ;; (agenda         . 10))))
+  (setq initial-buffer-choice (lambda () (get-buffer-create "*dashboard*")))
 
-  (setq dashboard-center-content t)
+  (setq dashboard-center-content t
+        dashboard-path-style 'truncate-middle)
+        ;; dashboard-page-separator "\n\f\n")
 
   (setq dashboard-items '((recents  . 5)
                         (bookmarks . 5)
-                        ; (projects . 5)
+                        (projects . 5)
                         ; (agenda . 5)
-                        (registers . 5)))
+                        ;; (registers . 5)
+                        ))
 
-  (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t)
+  ;; (setq dashboard-display-icons-p t)
+  ;; (setq dashboard-icon-type 'all-the-icons)
+
+  (setq dashboard-set-heading-icons t
+        dashboard-set-file-icons t)
+
+        ;; dashboard-projects-backend 'projectile
+        ;; dashboard-projects-switch-function #'counsel-projectile-switch-project-by-name)
+
+        ;; dashboard-heading-icons '((recents   . "nf-oct-history")
+        ;;                           (bookmarks . "nf-oct-bookmark")
+        ;;                           (agenda    . "nf-oct-calendar")
+        ;;                           (projects  . "nf-oct-briefcase")
+        ;;                           (registers . "nf-oct-database")))
 
   (dashboard-modify-heading-icons '((recents . "file-text")
                                     (bookmarks . "book")))
 
-
   (setq dashboard-set-navigator t)
-  ;; Format: "(icon title help action face prefix suffix)"
+
   (setq dashboard-navigator-buttons
-        `(;; line1
+        `(;; Line 1
           ((,(all-the-icons-octicon "mark-github" :height 1.1 :v-adjust 0.0)
             "Homepage"
             "Browse homepage"
             (lambda (&rest _) (browse-url "homepage")))
            ("★" "Star" "Show stars" (lambda (&rest _) (show-stars)) warning)
            ("?" "" "?/h" #'show-help nil "<" ">"))
-          ;; line 2
+          ;; Line 2
           ((,(all-the-icons-faicon "linkedin" :height 1.1 :v-adjust 0.0)
             "Linkedin"
             ""
@@ -47,12 +52,12 @@
   ;; Set the title
   (setq dashboard-banner-logo-title "I know no Evil")
   ;; Set the banner
-  ;; (setq dashboard-startup-banner 'logo)
+  (setq dashboard-startup-banner 'logo)
 
-  ;; (setq dashboard-items
-  ;;       '((recents   . 5)
-  ;;         (bookmarks . 5)
-  ;;         (project  . 5))))
+  ;; Dashboard footer
+  (setq dashboard-footer-icon
+        (all-the-icons-fileicon "emacs" :height 1.1 :v-adjust 0.0))
+
   (dashboard-setup-startup-hook))
 
 (provide 'env-dashboard)
