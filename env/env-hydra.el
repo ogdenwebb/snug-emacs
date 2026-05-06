7badfc4ce853200bdfb31f6b35cd3bd40ae7af58
ogdenwebb <ogdenwebb@gmail.com>
2026-05-06 20:45:29 +0300

    Add: File-info package

--- env/env-hydra.el
+++ env/env-hydra.el
@@ -62,4 +62,17 @@
       ("v" describe-variable "variable")
       ("i" info-lookup-symbol "info lookup")))))
 
+
+;; All necessary information about current file in hydra + posframe
+(use-package file-info
+  :bind (("C-c d" . 'file-info-show))
+  :config
+  (setq hydra-hint-display-type 'posframe)
+  (setq hydra-posframe-show-params `(:poshandler posframe-poshandler-frame-center
+                                               :internal-border-width 2
+                                               :internal-border-color "#61AFEF"
+                                               :left-fringe 16
+                                               :right-fringe 16)))
+
+
 (provide 'env-hydra)
