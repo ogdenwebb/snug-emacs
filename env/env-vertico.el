9075714202bc49a28155d59209f54d3b5bb4d872
ogdenwebb <ogdenwebb@gmail.com>
2026-05-17 15:56:32 +0300

    Update vertico & consult settings

--- env/env-vertico.el
+++ env/env-vertico.el
@@ -57,27 +57,52 @@
 (use-package orderless
   :after vertico
   :config
-  ;; (setq completion-styles '(basic partial-completion orderless)
-  ;;       completion-category-defaults nil
-  ;;       ;; completion-category-overrides '((file (styles basic partial-completion)))
+  ;; NOTE: Using Minad config from Consult wiki
+  (defun +orderless--consult-suffix ()
+    "Regexp which matches the end of string with Consult tofu support."
+    (if (boundp 'consult--tofu-regexp)
+        (concat consult--tofu-regexp "*\\'")
+      "\\'"))
 
-  ;;       ;; completion-category-overrides '((command (styles basic partial-completion))
-  ;;       ;;                                 (symbol (styles basic partial-completion))
-  ;;       ;;                                 (variable (styles basic partial-completion)))
-  ;;       )
+  ;; Recognizes the following patterns:
+  ;; * .ext (file extension)
+  ;; * regexp$ (regexp matching at end)
+  (defun +orderless-consult-dispatch (word _index _total)
+    (cond
+     ;; Ensure that $ works with Consult commands, which add disambiguation suffixes
+     ((string-suffix-p "$" word)
+      `(orderless-regexp . ,(concat (substring word 0 -1) (+orderless--consult-suffix))))
+     ;; File extensions
+     ((and (or minibuffer-completing-file-name
+               (derived-mode-p 'eshell-mode))
+           (string-match-p "\\`\\.." word))
+      `(orderless-regexp . ,(concat "\\." (substring word 1) (+orderless--consult-suffix))))))
 
-  ;; NOTE: Trying new settings
+  ;; Define orderless style with initialism by default
+  (orderless-define-completion-style +orderless-with-initialism
+    (orderless-matching-styles '(orderless-initialism orderless-literal orderless-regexp)))
+
+  ;; Certain dynamic completion tables (completion-table-dynamic) do not work
+  ;; properly with orderless. One can add basic as a fallback.  Basic will only
+  ;; be used when orderless fails, which happens only for these special
+  ;; tables. Also note that you may want to configure special styles for special
+  ;; completion categories, e.g., partial-completion for files.
   (setq completion-styles '(orderless basic)
-        orderless-matching-styles '(orderless-initialism
-                                    orderless-literal
-                                    orderless-regexp)
         completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion))))
-
-  ;; Exclude some commands
-  (defun without-orderless (fn & rest args)
-    (let ((completion-styles '(basic partial-completion)))
-      (apply fn args))))
+        ;;; Enable partial-completion for files.
+        ;;; Either give orderless precedence or partial-completion.
+        ;;; Note that completion-category-overrides is not really an override,
+        ;;; but rather prepended to the default completion-styles.
+        ;; completion-category-overrides '((file (styles orderless partial-completion))) ;; orderless is tried first
+        completion-category-overrides '((file (styles partial-completion)) ;; partial-completion is tried first
+                                        ;; enable initialism by default for symbols
+                                        (command (styles +orderless-with-initialism))
+                                        (variable (styles +orderless-with-initialism))
+                                        (symbol (styles +orderless-with-initialism)))
+        orderless-component-separator #'orderless-escapable-split-on-space ;; allow escaping space with backslash!
+        orderless-style-dispatchers (list #'+orderless-consult-dispatch
+                                          #'orderless-kwd-dispatch
+                                          #'orderless-affix-dispatch)))
 
 
 ;; Consulting completing-read
@@ -108,11 +133,6 @@
 ;; (use-package consult-vertico
 ;;   :ensure nil
 ;;   :after consult)
-
-;; Consult integration for projectile
-(use-package consult-projectile
-  :after (consult projectile)
-  :ensure (:repo "https://gitlab.com/OlMon/consult-projectile"))
 
 ;; Search and jump hl-todo keywords in buffers with consult.
 (use-package consult-todo
@@ -169,10 +189,10 @@
     ([remap yank-pop]                      #'consult-yank-pop)
     )
 
-  (use-package consult-projectile
-    :ensure nil
-    :general
-    ([remap projectile-find-file] #'consult-projectile))
+  ;; (use-package consult-projectile
+  ;;   :ensure nil
+  ;;   :general
+  ;;   ([remap projectile-find-file] #'consult-projectile))
   )
 
 ;; Embark
@@ -192,7 +212,7 @@
 
 ;; Display vertico in child-frame
 (use-package vertico-posframe
-             :disabled t
+  :disabled t
   :after (vertico)
   :preface
   (defun posframe-poshandler-frame-top-center-with-offset (info)
